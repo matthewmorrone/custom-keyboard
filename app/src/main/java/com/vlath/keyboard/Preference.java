e4254d9f8190e07bb832a459ed44c6663687941e
Matthew Morrone <matthewmorrone1@gmail.com>
2019-07-25 07:02:55 -0400

    Initial commit

---
+++ app/src/main/java/com/vlath/keyboard/Preference.java
@@ -0,0 +1,18 @@
+package com.vlath.keyboard;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Vlad on 6/20/2017.
+ */
+
+public class Preference extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle h) {
+        super.onCreate(h);
+        setContentView(R.layout.pref);
+        getFragmentManager().beginTransaction().replace(R.id.main, new PreferenceFragment()).commit();
+    }
+}
