cfac786b117be1bf95e7a69a26e89e97fcc045c7
Matthew Morrone <matthewmorrone1@gmail.com>
2019-09-30 13:44:45 -0400

    formatting

--- app/src/main/java/com/vlath/keyboard/PreferenceFragment.java
+++ app/src/main/java/com/vlath/keyboard/PreferenceFragment.java
@@ -5,74 +5,74 @@ import android.os.Bundle;
 import android.preference.*;
 
 public class PreferenceFragment extends android.preference.PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
-
-  ListPreference listTheme;
-
-  EditTextPreference k1;
-  EditTextPreference k2;
-  EditTextPreference k3;
-  EditTextPreference k4;
-  EditTextPreference k5;
-  EditTextPreference k6;
-  EditTextPreference k7;
-  EditTextPreference k8;
-
-  EditTextPreference popup1;
-  EditTextPreference popup2;
-  
-  @Override
-  public void onCreate(Bundle s) {
-    super.onCreate(s);
-    addPreferencesFromResource(R.xml.ime_preferences);
-    listTheme = (ListPreference)findPreference("theme");
-    listTheme.setSummary(listTheme.getEntry());
-
-    k1 = (EditTextPreference)findPreference("k1");
-    k2 = (EditTextPreference)findPreference("k2");
-    k3 = (EditTextPreference)findPreference("k3");
-    k4 = (EditTextPreference)findPreference("k4");
-    k5 = (EditTextPreference)findPreference("k5");
-    k6 = (EditTextPreference)findPreference("k6");
-    k7 = (EditTextPreference)findPreference("k7");
-    k8 = (EditTextPreference)findPreference("k8");
-
-    popup1 = (EditTextPreference)findPreference("popup1");
-    popup2 = (EditTextPreference)findPreference("popup2");
-
-    k1.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k1", ""));
-    k2.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k2", ""));
-    k3.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k3", ""));
-    k4.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k4", ""));
-    k5.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k5", ""));
-    k6.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k6", ""));
-    k7.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k7", ""));
-    k8.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k8", ""));
     
-    popup1.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("popup1", ""));
-    popup2.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("popup2", ""));
-
-    PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).registerOnSharedPreferenceChangeListener(this);
-  }
-
-  @Override
-  public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-    // addPreferencesFromResource(R.xml.ime_preferences);
-    listTheme = (ListPreference)findPreference("theme");
-    listTheme.setSummary(listTheme.getEntry());
-
-    try {
-      k1.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k1", ""));
-      k2.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k2", ""));
-      k3.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k3", ""));
-      k4.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k4", ""));
-      k5.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k5", ""));
-      k6.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k6", ""));
-      k7.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k7", ""));
-      k8.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k8", ""));
+    ListPreference listTheme;
     
-      popup1.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("popup1", ""));
-      popup2.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("popup2", ""));
+    EditTextPreference k1;
+    EditTextPreference k2;
+    EditTextPreference k3;
+    EditTextPreference k4;
+    EditTextPreference k5;
+    EditTextPreference k6;
+    EditTextPreference k7;
+    EditTextPreference k8;
+    
+    EditTextPreference popup1;
+    EditTextPreference popup2;
+    
+    @Override
+    public void onCreate(Bundle s) {
+        super.onCreate(s);
+        addPreferencesFromResource(R.xml.ime_preferences);
+        listTheme = (ListPreference)findPreference("theme");
+        listTheme.setSummary(listTheme.getEntry());
+        
+        k1 = (EditTextPreference)findPreference("k1");
+        k2 = (EditTextPreference)findPreference("k2");
+        k3 = (EditTextPreference)findPreference("k3");
+        k4 = (EditTextPreference)findPreference("k4");
+        k5 = (EditTextPreference)findPreference("k5");
+        k6 = (EditTextPreference)findPreference("k6");
+        k7 = (EditTextPreference)findPreference("k7");
+        k8 = (EditTextPreference)findPreference("k8");
+        
+        popup1 = (EditTextPreference)findPreference("popup1");
+        popup2 = (EditTextPreference)findPreference("popup2");
+        
+        k1.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k1", ""));
+        k2.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k2", ""));
+        k3.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k3", ""));
+        k4.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k4", ""));
+        k5.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k5", ""));
+        k6.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k6", ""));
+        k7.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k7", ""));
+        k8.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k8", ""));
+        
+        popup1.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("popup1", ""));
+        popup2.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("popup2", ""));
+        
+        PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).registerOnSharedPreferenceChangeListener(this);
     }
-    catch (NullPointerException ignored) {}
-  }
+    
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        // addPreferencesFromResource(R.xml.ime_preferences);
+        listTheme = (ListPreference)findPreference("theme");
+        listTheme.setSummary(listTheme.getEntry());
+        
+        try {
+            k1.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k1", ""));
+            k2.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k2", ""));
+            k3.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k3", ""));
+            k4.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k4", ""));
+            k5.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k5", ""));
+            k6.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k6", ""));
+            k7.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k7", ""));
+            k8.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("k8", ""));
+            
+            popup1.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("popup1", ""));
+            popup2.setTitle(PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).getString("popup2", ""));
+        }
+        catch (NullPointerException ignored) {}
+    }
 }
