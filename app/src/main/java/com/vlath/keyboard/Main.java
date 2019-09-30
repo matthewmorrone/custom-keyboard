f3e4470f8a8529d2c0b35432353f8776b9ced6c5
Matthew Morrone <matthewmorrone1@gmail.com>
2019-09-30 13:43:54 -0400

    formatting

--- app/src/main/java/com/vlath/keyboard/Main.java
+++ app/src/main/java/com/vlath/keyboard/Main.java
@@ -12,30 +12,30 @@ import android.widget.Toast;
 
 public class Main extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activate);
-    TextView t2 = findViewById(R.id.textView2);
-    t2.setMovementMethod(LinkMovementMethod.getInstance());
-  }
-
-  public void settings(View v) {
-    Intent intent = new Intent(this, Preference.class);
-    startActivity(intent);
-  }
-
-  public void enable(View v) {
-    this.startActivity(new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS));
-  }
-
-  public void select(View v) {
-    InputMethodManager imeManager = (InputMethodManager)getApplicationContext().getSystemService(INPUT_METHOD_SERVICE);
-    if (imeManager != null) {
-      imeManager.showInputMethodPicker();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activate);
+        TextView t2 = findViewById(R.id.textView2);
+        t2.setMovementMethod(LinkMovementMethod.getInstance());
     }
-    else {
-      Toast.makeText(this, "Not possible", Toast.LENGTH_LONG).show();
+
+    public void settings(View v) {
+        Intent intent = new Intent(this, Preference.class);
+        startActivity(intent);
     }
-  }
+
+    public void enable(View v) {
+        this.startActivity(new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS));
+    }
+
+    public void select(View v) {
+        InputMethodManager imeManager = (InputMethodManager)getApplicationContext().getSystemService(INPUT_METHOD_SERVICE);
+        if (imeManager != null) {
+            imeManager.showInputMethodPicker();
+        }
+        else {
+            Toast.makeText(this, "Not possible", Toast.LENGTH_LONG).show();
+        }
+    }
 }
