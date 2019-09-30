a4e947829cc3bb2b3a5c0430eca199cda372c71f
Matthew Morrone <matthewmorrone1@gmail.com>
2019-09-30 13:47:26 -0400

    formatting

--- app/src/main/java/com/vlath/keyboard/Themes.java
+++ app/src/main/java/com/vlath/keyboard/Themes.java
@@ -1,52 +1,52 @@
 package com.vlath.keyboard;
 
 public class Themes {
-    	public static final float[] sNoneColorArray = {
-        1.0f, 0.0f, 0.0f, 0.0f,   0, // red
-        0.0f, 1.0f, 0.0f, 0.0f,   0, // green
-        0.0f, 0.0f, 1.0f, 0.0f,   0, // blue
-        0.0f, 0.0f, 0.0f, 1.0f,   0  // alpha
+    public static final float[] sNoneColorArray = {
+         1.0f, 0.0f, 0.0f, 0.0f,   0, // red
+         0.0f, 1.0f, 0.0f, 0.0f,   0, // green
+         0.0f, 0.0f, 1.0f, 0.0f,   0, // blue
+         0.0f, 0.0f, 0.0f, 1.0f,   0  // alpha
     };
     public static final float[] sNegativeColorArray = {
-       -1.0f, 0.0f, 0.0f, 0.0f, 255, // red
-        0.0f,-1.0f, 0.0f, 0.0f, 255, // green
-        0.0f, 0.0f,-1.0f, 0.0f, 255, // blue
-        0.0f, 0.0f, 0.0f, 1.0f,   0  // alpha
+        -1.0f, 0.0f, 0.0f, 0.0f, 255, // red
+         0.0f,-1.0f, 0.0f, 0.0f, 255, // green
+         0.0f, 0.0f,-1.0f, 0.0f, 255, // blue
+         0.0f, 0.0f, 0.0f, 1.0f,   0  // alpha
     };
     public static final float[] sBlueBlackColorArray = {
-       -1.0f, 0.0f, 0.0f, 0.0f,  41, // red
-        0.0f,-1.0f, 0.0f, 0.0f, 128, // green
-        0.0f, 0.0f,-1.0f, 0.0f, 185, // blue
-        0.0f, 0.0f, 0.0f, 1.0f,   0  // alpha
+        -1.0f, 0.0f, 0.0f, 0.0f,  41, // red
+         0.0f,-1.0f, 0.0f, 0.0f, 128, // green
+         0.0f, 0.0f,-1.0f, 0.0f, 185, // blue
+         0.0f, 0.0f, 0.0f, 1.0f,   0  // alpha
     };
     public static final float[] sBlueWhiteColorArray = {
-        1.0f, 0.0f, 0.0f, 0.0f,  41, // red
-        0.0f, 1.0f, 0.0f, 0.0f, 128, // green
-        0.0f, 0.0f, 1.0f, 0.0f, 185, // blue
-        0.0f, 0.0f, 0.0f, 1.0f,   1  // alpha
+         1.0f, 0.0f, 0.0f, 0.0f,  41, // red
+         0.0f, 1.0f, 0.0f, 0.0f, 128, // green
+         0.0f, 0.0f, 1.0f, 0.0f, 185, // blue
+         0.0f, 0.0f, 0.0f, 1.0f,   1  // alpha
     };
     public static final float[] sRedWhiteColorArray = {
-        1.0f, 0.0f, 0.0f, 0.0f, 192, // red
-        0.0f, 1.0f, 0.0f, 0.0f,  57, // green
-        0.0f, 0.0f, 1.0f, 0.0f,  43, // blue
-        0.0f, 0.0f, 0.0f, 1.0f,   0  // alpha
+         1.0f, 0.0f, 0.0f, 0.0f, 192, // red
+         0.0f, 1.0f, 0.0f, 0.0f,  57, // green
+         0.0f, 0.0f, 1.0f, 0.0f,  43, // blue
+         0.0f, 0.0f, 0.0f, 1.0f,   0  // alpha
     };
     public static final float[] sRedBlackColorArray = {
-       -1.0f, 0.0f, 0.0f, 0.0f, 192, // red
-        0.0f,-1.0f, 0.0f, 0.0f,  57, // green
-        0.0f, 0.0f,-1.0f, 0.0f,  43, // blue
-        0.0f, 0.0f, 0.0f, 1.0f,   0  // alpha
+        -1.0f, 0.0f, 0.0f, 0.0f, 192, // red
+         0.0f,-1.0f, 0.0f, 0.0f,  57, // green
+         0.0f, 0.0f,-1.0f, 0.0f,  43, // blue
+         0.0f, 0.0f, 0.0f, 1.0f,   0  // alpha
     };
     public static final float[] sOrangeBlackColorArray = {
-        1.0f, 0.0f, 0.0f, 0.0f, 230, // red
-        0.0f, 1.0f, 0.0f, 0.0f, 126, // green
-        0.0f, 0.0f, 1.0f, 0.0f,  34, // blue
-        0.0f, 0.0f, 0.0f, 1.0f,   0  // alpha
+         1.0f, 0.0f, 0.0f, 0.0f, 230, // red
+         0.0f, 1.0f, 0.0f, 0.0f, 126, // green
+         0.0f, 0.0f, 1.0f, 0.0f,  34, // blue
+         0.0f, 0.0f, 0.0f, 1.0f,   0  // alpha
     };
     public static final float[] sMaterialDarkColorArray = {
-        1.0f, 0.0f, 0.0f, 0.0f,  55, // red
-        0.0f, 1.0f, 0.0f, 0.0f,  71, // green
-        0.0f, 0.0f, 1.0f, 0.0f,  79, // blue
-        0.0f, 0.0f, 0.0f, 1.0f,   1  // alpha
+         1.0f, 0.0f, 0.0f, 0.0f,  55, // red
+         0.0f, 1.0f, 0.0f, 0.0f,  71, // green
+         0.0f, 0.0f, 1.0f, 0.0f,  79, // blue
+         0.0f, 0.0f, 0.0f, 1.0f,   1  // alpha
     };
 }
