f7feba7a156ec2a5aef3ac46266a8687a217e99e
Matthew Morrone <matthewmorrone1@gmail.com>
2020-09-16 00:52:46 -0400

    autocommit

--- app/src/main/java/com/custom/keyboard/Main.java
+++ app/src/main/java/com/custom/keyboard/Main.java
@@ -1,40 +1,43 @@
 package com.custom.keyboard;
 
-import android.content.Context;
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.provider.Settings;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 
-public class Main extends AppCompatActivity {
-
-    private ViewPager viewPager;
-    private MyViewPagerAdapter myViewPagerAdapter;
-    private LinearLayout dotsLayout;
-    private TextView[] dots;
-    private int[] layouts;
-    private Button btnSkip, btnNext;
+public class Main extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activate);
+    }
+
+    public void settings(View v) {
+        Intent intent = new Intent(this, Preference.class);
+        startActivity(intent);
+    }
+
+    public void enable(View v) {
+        this.startActivity(new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS));
+    }
+
+    public void select(View v) {
+        InputMethodManager imeManager = (InputMethodManager)getApplicationContext().getSystemService(INPUT_METHOD_SERVICE);
+        if (imeManager != null) {
+            imeManager.showInputMethodPicker();
+        }
+        else {
+            Toast.makeText(this, "Not possible", Toast.LENGTH_LONG).show();
+        }
     }
 
     private void launchHomeScreen() {
@@ -43,9 +46,6 @@ public class Main extends AppCompatActivity {
         startActivity(intent);
     }
 
-    /**
-     * Making notification bar transparent
-     */
     private void changeStatusBarColor(boolean on) {
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
@@ -57,43 +57,7 @@ public class Main extends AppCompatActivity {
         }
     }
 
-    /**
-     * View pager adapter
-     */
-    public class MyViewPagerAdapter extends PagerAdapter {
-        private LayoutInflater layoutInflater;
-
-        public MyViewPagerAdapter() {
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            layoutInflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View view = null;
-            if (layoutInflater != null) {
-                view = layoutInflater.inflate(layouts[position], container, false);
-            }
-            container.addView(view);
-            return view;
-        }
-
-        @Override
-        public int getCount() {
-            return layouts.length;
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object obj) {
-            return view == obj;
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            View view = (View)object;
-            container.removeView(view);
-        }
-    }
-
+    /*
     public boolean getPresentationShown() {
         try {
             return PreferenceManager.getDefaultSharedPreferences(this).getBoolean("presentation", false);
@@ -106,6 +70,7 @@ public class Main extends AppCompatActivity {
     public void setPresentationShown() {
         PreferenceManager.getDefaultSharedPreferences(this).edit().putBoolean("presentation", true).apply();
     }
+    */
 
     /*
     public void showTwitterDialog() {
@@ -133,24 +98,6 @@ public class Main extends AppCompatActivity {
     */
 
 
-    public void settings(View v) {
-        Intent intent = new Intent(this, Preference.class);
-        startActivity(intent);
-    }
-
-    public void enable(View v) {
-        this.startActivity(new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS));
-    }
-
-    public void select(View v) {
-        InputMethodManager imeManager = (InputMethodManager)getApplicationContext().getSystemService(INPUT_METHOD_SERVICE);
-        if (imeManager != null) {
-            imeManager.showInputMethodPicker();
-        }
-        else {
-            Toast.makeText(this, "Not possible", Toast.LENGTH_LONG).show();
-        }
-    }
 
 
 }
