bcc524dbe0954376ae371a844931d21f2959d858
Matthew Morrone <matthewmorrone1@gmail.com>
2020-09-16 00:54:22 -0400

    autocommit

--- app/src/main/java/com/custom/keyboard/Preference.java
+++ app/src/main/java/com/custom/keyboard/Preference.java
@@ -1,10 +1,11 @@
 package com.custom.keyboard;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Preference extends AppCompatActivity {
+public class Preference extends Activity {
     @Override
     public void onCreate(Bundle h) {
         super.onCreate(h);
